5555c5119e72b7e2594d1555dd4d9f42434f292d
Faizulmd13 <faizulmd1306@gmail.com>
2025-09-26 19:08:11 +0530

    refactor by creating html elements instead of pasting html inside htmlContent

--- src/about.js
+++ src/about.js
@@ -1,17 +1,35 @@
-export default function loadAbout(){
-    const main = document.querySelector("main");
-    main.innerHTML = `
-    <div class="about-header">
-        <h1 class="title-text">About Us</h1>
-        <p class="subtitle-text">Our story, our passion, our commitment</p>
-        <img
-          src="https://images.unsplash.com/flagged/photo-1561350117-501b4661f8d4?q=80&w=870&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.1.0&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          class="page-image"
-        />
-      </div>
+export default function loadAbout() {
+  const main = document.querySelector("main");
 
-      <p id="about-desc">
-        <strong>Maaran Parotta Shop</strong> began as a dream of bringing
+  const aboutHeader = document.createElement("div");
+  aboutHeader.classList.add("about-header");
+
+  const titleText = document.createElement("h1");
+  titleText.classList.add("title-text");
+  titleText.textContent = "About Us";
+
+  const subtitleText = document.createElement("p");
+  subtitleText.classList.add("subtitle-text");
+  subtitleText.textContent = "Our story, our passion, our commitment";
+
+  const pageImage = document.createElement("img");
+  pageImage.classList.add("page-image");
+  pageImage.src =
+    "https://images.unsplash.com/flagged/photo-1561350117-501b4661f8d4?q=80&w=870&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.1.0&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
+  aboutHeader.appendChild(titleText);
+  aboutHeader.appendChild(subtitleText);
+  aboutHeader.appendChild(pageImage);
+
+  const aboutDesc = document.createElement("p");
+  aboutDesc.id = "about-desc";
+
+  const shopName = document.createElement("strong");
+  shopName.textContent = "Maaran Parotta Shop";
+
+  aboutDesc.appendChild(shopName);
+
+  aboutDesc.textContent += ` began as a dream of bringing
         authentic South Indian flavors to our beloved neighborhood. Founded in
         2010 by the Maaran family, who have been making parotta for generations,
         our shop has become a favorite spot for locals and visitors alike. Our
@@ -23,7 +41,8 @@ export default function loadAbout(){
         our philosophy that food should be a celebration of home, family, and
         tradition. Our rustic décor, the aroma of freshly cooked parotta, and
         the friendly service create a cozy experience that feels like a visit to
-        a traditional South Indian kitchen.
-      </p>
-    `;
+        a traditional South Indian kitchen.`;
+
+  main.appendChild(aboutHeader);
+  main.appendChild(aboutDesc);
 }
