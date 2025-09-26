495c8c4503a9d12606f9f7232095048fe0451aeb
Faizulmd13 <faizulmd1306@gmail.com>
2025-09-26 19:09:20 +0530

    refactor by creating html elements instead of pasting html inside htmlContent

--- src/home.js
+++ src/home.js
@@ -1,18 +1,34 @@
-export default function loadHome(){
-    const main = document.querySelector("main");
-    main.innerHTML = `
-    <div class="main-header">
-        <h1 class="title-text">Maaran Parotta Shop</h1>
-        <p class="subtitle-text">Authentic parotta in Madurai</p>
-        <img
-          src="https://images.unsplash.com/photo-1536305030588-45dc07a2a372?q=80&w=870&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.1.0&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt="Restaurant Interior"
-          class="page-image"
-        />
-      </div>
+export default function loadHome() {
+  const main = document.querySelector("main");
 
-      <p id="home-desc">
-        Welcome to <strong>Maaran Parotta Shop</strong>, where traditional South
+  const mainHeader = document.createElement("div");
+  mainHeader.classList.add("main-header");
+
+  const titleText = document.createElement("h1");
+  titleText.classList.add("title-text");
+  titleText.textContent = "Maaran Parotta Shop";
+
+  const subtitleText = document.createElement("p");
+  subtitleText.classList.add("subtitle-text");
+  subtitleText.textContent = "Authentic parotta in Madurai";
+
+  const pageImage = document.createElement("img");
+  pageImage.classList.add("page-image");
+  pageImage.src =
+    "https://images.unsplash.com/photo-1536305030588-45dc07a2a372?q=80&w=870&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.1.0&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  pageImage.alt = "Restaurant Interior";
+
+  mainHeader.appendChild(titleText);
+  mainHeader.appendChild(subtitleText);
+  mainHeader.appendChild(pageImage);
+
+  const homeDesc = document.createElement("p");
+  homeDesc.id = "home-desc";
+
+  const shopName = document.createElement("strong");
+  shopName.textContent = "Maaran Parotta Shop";
+
+  homeDesc.textContent = ` Welcome to ${shopName}, where traditional South
         Indian flavors meet the comforting simplicity of home-cooked meals.
         Nestled in the heart of our neighborhood, our shop offers a warm and
         welcoming experience with the aroma of freshly made parotta and the
@@ -22,6 +38,8 @@ export default function loadHome(){
         flavors, made with love and tradition. Whether you're stopping by for a
         quick bite, a family meal, or a comforting snack, Maaran Parotta Shop
         promises a taste of home and a truly satisfying experience.
-      </p>
-    `;
+`;
+
+  main.appendChild(mainHeader);
+  main.appendChild(homeDesc);
 }
